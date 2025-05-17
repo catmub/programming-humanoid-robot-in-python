ffdfdcb9b8fed4c622dff7f98e319a5aca78c82d
wsl <tuandat.hnvn@gmail.com>
2025-05-17 12:10:03 +0200

    wsl

--- kinematics/forward_kinematics.py
+++ kinematics/forward_kinematics.py
@@ -25,6 +25,7 @@ from numpy.matlib import matrix, identity
 
 from recognize_posture import PostureRecognitionAgent
 
+import numpy as np
 
 class ForwardKinematicsAgent(PostureRecognitionAgent):
     def __init__(self, simspark_ip='localhost',
@@ -36,10 +37,109 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
         self.transforms = {n: identity(4) for n in self.joint_names}
 
         # chains defines the name of chain and joints of the chain
-        self.chains = {'Head': ['HeadYaw', 'HeadPitch']
+        self.chains = {'Head': ['HeadYaw', 'HeadPitch'],
                        # YOUR CODE HERE
+                       'LArm': ['LShoulderPitch', 'LShoulderRoll', 'LElbowYaw', 'LElbowRoll', 'LWristYaw'],
+                       'LLeg': ['LHipYawPitch', 'LHipRoll', 'LHipPitch', 'LKneePitch', 'LAnklePitch', 'LAnkleRoll'],
+                       'RLeg': ['RHipYawPitch', 'RHipRoll', 'RHipPitch', 'RKneePitch', 'RAnklePitch', 'RAnkleRoll'],
+                       'RArm': ['RShoulderPitch', 'RShoulderRoll', 'RElbowYaw', 'RElbowRoll', 'RWristYaw']
                        }
+        self.link_offsets = {
+        # Head
+        'HeadYaw': [0, 0, 0.1265],      
+        'HeadPitch': [0, 0, 0],        
+    
+        # Left Arm
+        'LShoulderPitch': [0.0, 0.098, 0.100],    
+        'LShoulderRoll': [0.0, 0.0, 0.0],    
+        'LElbowYaw': [0.105, 0.015, 0.0],         
+        'LElbowRoll': [0.0, 0.0, 0.0],        
+        'LWristYaw': [0.05595, 0.0, 0.0],        
+     
+    
+        # Right Arm (mirror of Left)
+        'RShoulderPitch': [0.0, -0.098, 0.100],
+        'RShoulderRoll': [0.0, 0.0, 0.0],
+        'RElbowYaw': [0.105, -0.015, 0.0],
+        'RElbowRoll': [0.0, 0.0, 0.0],
+        'RWristYaw': [0.05595, 0.0, 0.0],
+    
+        # Left Leg
+        'LHipYawPitch': [0.0, 0.050, -0.085],     
+        'LHipRoll': [0.0, 0.0, 0.0],
+        'LHipPitch': [0.0, 0.0, 0.0],
+        'LKneePitch': [0.0, 0.0, -0.100],
+        'LAnklePitch': [0.0, 0.0, -0.1029],
+        'LAnkleRoll': [0.0, 0.0, 0.0],            
+    
+        # Right Leg (mirror of Left)
+        'RHipYawPitch': [0.0, -0.050, -0.085],
+        'RHipRoll': [0.0, 0.0, 0.0],
+        'RHipPitch': [0.0, 0.0, 0.0],
+        'RKneePitch': [0.0, 0.0, -0.100],
+        'RAnklePitch': [0.0, 0.0, -0.1029],
+        'RAnkleRoll': [0.0, 0.0, 0.0],
+        }
 
+        self.joint_axis= {
+        'HeadYaw': [0, 0, 1],
+        'HeadPitch': [0, 1, 0],
+
+        'RShoulderPitch': [0, 1, 0],
+        'RShoulderRoll': [0, 0, 1],
+        'RElbowYaw': [1, 0, 0],
+        'RElbowRoll': [0, 0, 1],
+        'RWristYaw' : [0, 0, 1], #??? or it might be 1,1,1
+            
+        'LShoulderPitch': [0, 1, 0],
+        'LShoulderRoll': [0, 0, 1],
+        'LElbowYaw': [0, 1, 0],
+        'LElbowRoll': [0, 0, 1],
+        'LWristYaw' : [0, 0, 1], #??? or it might be 1,1,1
+
+        'LHipYawPitch': [0, 0, 1],
+        'RHipYawPitch': [0, 0, 1],
+        
+        'LHipRoll': [1, 0, 0],
+        'LHipPitch': [0, 1, 0],
+        'LKneePitch': [0, 1, 0],
+        'LAnklePitch': [0, 1, 0],
+        'LAnkleRoll': [1, 0, 0],
+        
+        'RHipRoll': [1, 0, 0],
+        'RHipPitch': [0, 1, 0],
+        'RKneePitch': [0, 1, 0],
+        'RAnklePitch': [0, 1, 0],
+        'RAnkleRoll': [1, 0, 0],
+        }
+
+    #help function
+    def rotation_matrix(axis, angle):
+    
+        axis = tuple(axis)
+        c, s = np.cos(angle), np.sin(angle)
+        if axis == (1, 0, 0):  # X
+            return np.array([
+                [1, 0, 0],
+                [0, c, -s],
+                [0, s, c]
+            ])
+        elif axis == (0, 1, 0):  # Y
+            return np.array([
+                [c, 0, s],
+                [0, 1, 0],
+                [-s, 0, c]
+            ])
+        elif axis == (0, 0, 1):  # Z
+            return np.array([
+                [c, -s, 0],
+                [s, c, 0],
+                [0, 0, 1]
+            ])
+        else:
+            raise ValueError("Unsupported rotation axis")
+    #----
+    
     def think(self, perception):
         self.forward_kinematics(perception.joint)
         return super(ForwardKinematicsAgent, self).think(perception)
@@ -54,7 +154,21 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
         '''
         T = identity(4)
         # YOUR CODE HERE
-
+        # 1. Get fixed offset
+        offset = self.link_offsets.get(joint_name, [0, 0, 0])
+        T_offset = identity(4)
+        T_offset[0:3, 3] = offset
+    
+        # 2. Get rotation
+        axis = self.joint_axes.get(joint_name)
+        if axis is None:
+            raise KeyError(f"Missing axis definition for joint {joint_name}")
+        R = rotation_matrix(axis, joint_angle)
+        T_rotate = identity(4)
+        T_rotate[0:3, 0:3] = R
+    
+        # 3. Combine: Translate then rotate
+        T = T_offset @ T_rotate
         return T
 
     def forward_kinematics(self, joints):
@@ -68,7 +182,10 @@ class ForwardKinematicsAgent(PostureRecognitionAgent):
                 angle = joints[joint]
                 Tl = self.local_trans(joint, angle)
                 # YOUR CODE HERE
-
+                for joint in chain:
+                    T_local = self.local_trans(joint, joint_angles[joint])
+                    T = T @ T_local
+    
                 self.transforms[joint] = T
 
 if __name__ == '__main__':
